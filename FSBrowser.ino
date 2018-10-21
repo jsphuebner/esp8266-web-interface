b03e48354e5a7777a52ec02a2bc8eb06b1067f87
jsphuebner <dev@johanneshuebner.com>
2018-10-21 23:03:00 +0200

    First commit

---
+++ FSBrowser.ino
@@ -0,0 +1,436 @@
+/* 
+  FSWebServer - Example WebServer with SPIFFS backend for esp8266
+  Copyright (c) 2015 Hristo Gochkov. All rights reserved.
+  This file is part of the ESP8266WebServer library for Arduino environment.
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+  
+  upload the contents of the data folder with MkSPIFFS Tool ("ESP8266 Sketch Data Upload" in Tools menu in Arduino IDE)
+  or you can upload the contents of a folder if you CD in that folder and run the following command:
+  for file in `ls -A1`; do curl -F "file=@$PWD/$file" esp8266fs.local/edit; done
+  
+  access the sample web page at http://esp8266fs.local
+  edit the page by going to http://esp8266fs.local/edit
+*/
+/*
+ * This file is part of the esp8266 web interface
+ *
+ * Copyright (C) 2018 Johannes Huebner <dev@johanneshuebner.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+#include <FS.h>
+#include <Ticker.h>
+
+#define DBG_OUTPUT_PORT Serial
+
+const char* host = "inverter";
+bool fastUart = false;
+bool fastUartAvailable = true;
+
+ESP8266WebServer server(80);
+//holds the current upload
+File fsUploadFile;
+Ticker sta_tick;
+
+//format bytes
+String formatBytes(size_t bytes){
+  if (bytes < 1024){
+    return String(bytes)+"B";
+  } else if(bytes < (1024 * 1024)){
+    return String(bytes/1024.0)+"KB";
+  } else if(bytes < (1024 * 1024 * 1024)){
+    return String(bytes/1024.0/1024.0)+"MB";
+  } else {
+    return String(bytes/1024.0/1024.0/1024.0)+"GB";
+  }
+}
+
+String getContentType(String filename){
+  if(server.hasArg("download")) return "application/octet-stream";
+  else if(filename.endsWith(".htm")) return "text/html";
+  else if(filename.endsWith(".html")) return "text/html";
+  else if(filename.endsWith(".css")) return "text/css";
+  else if(filename.endsWith(".js")) return "application/javascript";
+  else if(filename.endsWith(".png")) return "image/png";
+  else if(filename.endsWith(".gif")) return "image/gif";
+  else if(filename.endsWith(".jpg")) return "image/jpeg";
+  else if(filename.endsWith(".ico")) return "image/x-icon";
+  else if(filename.endsWith(".xml")) return "text/xml";
+  else if(filename.endsWith(".pdf")) return "application/x-pdf";
+  else if(filename.endsWith(".zip")) return "application/x-zip";
+  else if(filename.endsWith(".gz")) return "application/x-gzip";
+  return "text/plain";
+}
+
+bool handleFileRead(String path){
+  //DBG_OUTPUT_PORT.println("handleFileRead: " + path);
+  if(path.endsWith("/")) path += "index.html";
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if(SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)){
+    if(SPIFFS.exists(pathWithGz))
+      path += ".gz";
+    File file = SPIFFS.open(path, "r");
+    size_t sent = server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
+void handleFileUpload(){
+  if(server.uri() != "/edit") return;
+  HTTPUpload& upload = server.upload();
+  if(upload.status == UPLOAD_FILE_START){
+    String filename = upload.filename;
+    if(!filename.startsWith("/")) filename = "/"+filename;
+    //DBG_OUTPUT_PORT.print("handleFileUpload Name: "); DBG_OUTPUT_PORT.println(filename);
+    fsUploadFile = SPIFFS.open(filename, "w");
+    filename = String();
+  } else if(upload.status == UPLOAD_FILE_WRITE){
+    //DBG_OUTPUT_PORT.print("handleFileUpload Data: "); DBG_OUTPUT_PORT.println(upload.currentSize);
+    if(fsUploadFile)
+      fsUploadFile.write(upload.buf, upload.currentSize);
+  } else if(upload.status == UPLOAD_FILE_END){
+    if(fsUploadFile)
+      fsUploadFile.close();
+    //DBG_OUTPUT_PORT.print("handleFileUpload Size: "); DBG_OUTPUT_PORT.println(upload.totalSize);
+  }
+}
+
+void handleFileDelete(){
+  if(server.args() == 0) return server.send(500, "text/plain", "BAD ARGS");
+  String path = server.arg(0);
+  //DBG_OUTPUT_PORT.println("handleFileDelete: " + path);
+  if(path == "/")
+    return server.send(500, "text/plain", "BAD PATH");
+  if(!SPIFFS.exists(path))
+    return server.send(404, "text/plain", "FileNotFound");
+  SPIFFS.remove(path);
+  server.send(200, "text/plain", "");
+  path = String();
+}
+
+void handleFileCreate(){
+  if(server.args() == 0)
+    return server.send(500, "text/plain", "BAD ARGS");
+  String path = server.arg(0);
+  DBG_OUTPUT_PORT.println("handleFileCreate: " + path);
+  if(path == "/")
+    return server.send(500, "text/plain", "BAD PATH");
+  if(SPIFFS.exists(path))
+    return server.send(500, "text/plain", "FILE EXISTS");
+  File file = SPIFFS.open(path, "w");
+  if(file)
+    file.close();
+  else
+    return server.send(500, "text/plain", "CREATE FAILED");
+  server.send(200, "text/plain", "");
+  path = String();
+}
+
+void handleFileList() {
+  String path = "/";
+  if(server.hasArg("dir")) 
+    String path = server.arg("dir");
+  //DBG_OUTPUT_PORT.println("handleFileList: " + path);
+  Dir dir = SPIFFS.openDir(path);
+  path = String();
+
+  String output = "[";
+  while(dir.next()){
+    File entry = dir.openFile("r");
+    if (output != "[") output += ',';
+    bool isDir = false;
+    output += "{\"type\":\"";
+    output += (isDir)?"dir":"file";
+    output += "\",\"name\":\"";
+    output += String(entry.name()).substring(1);
+    output += "\"}";
+    entry.close();
+  }
+  
+  output += "]";
+  server.send(200, "text/json", output);
+}
+
+static void sendCommand(String cmd)
+{
+  Serial.print("\n");
+  delay(1);
+  while(Serial.available())
+    Serial.read(); //flush all previous output
+  Serial.print(cmd);
+  Serial.print("\n");
+  Serial.readStringUntil('\n'); //consume echo  
+}
+
+static void handleCommand() {
+  const int cmdBufSize = 128;
+  if(!server.hasArg("cmd")) {server.send(500, "text/plain", "BAD ARGS"); return;}
+
+  String cmd = server.arg("cmd").substring(0, cmdBufSize);
+  int repeat = 0;
+  char buffer[255];
+  size_t len = 0;
+  String output;
+
+  if (server.hasArg("repeat"))
+    repeat = server.arg("repeat").toInt();
+
+  if (!fastUart && fastUartAvailable)
+  {
+    sendCommand("fastuart");
+    if (Serial.readString().startsWith("OK"))
+    {
+      Serial.begin(921600);
+      fastUart = true;
+    }
+    {
+      fastUartAvailable = false;
+    }
+  }
+
+  sendCommand(cmd);
+  do {
+    memset(buffer,0,sizeof(buffer));
+    len = Serial.readBytes(buffer, sizeof(buffer) - 1);
+    output += buffer;
+
+    if (repeat)
+    {
+      repeat--;
+      Serial.print("!");
+      Serial.readBytes(buffer, 1); //consume "!"
+    }
+  } while (len > 0);
+  server.send(200, "text/json", output);
+}
+
+static uint32_t crc32_word(uint32_t Crc, uint32_t Data)
+{
+  int i;
+
+  Crc = Crc ^ Data;
+
+  for(i=0; i<32; i++)
+    if (Crc & 0x80000000)
+      Crc = (Crc << 1) ^ 0x04C11DB7; // Polynomial used in STM32
+    else
+      Crc = (Crc << 1);
+
+  return(Crc);
+}
+
+static uint32_t crc32(uint32_t* data, uint32_t len, uint32_t crc)
+{
+   for (uint32_t i = 0; i < len; i++)
+      crc = crc32_word(crc, data[i]);
+   return crc;
+}
+
+
+static void handleUpdate()
+{
+  if(!server.hasArg("step") || !server.hasArg("file")) {server.send(500, "text/plain", "BAD ARGS"); return;}
+  const size_t PAGE_SIZE_BYTES = 1024;
+  int step = server.arg("step").toInt();
+  File file = SPIFFS.open(server.arg("file"), "r");
+  int pages = (file.size() + PAGE_SIZE_BYTES - 1) / PAGE_SIZE_BYTES;
+  String message;
+
+  if (step == -1)
+  {
+    int c;
+    sendCommand("reset");
+
+    if (fastUart)
+    {
+      Serial.begin(115200);
+      fastUart = false;
+      fastUartAvailable = true; //retry after reboot
+    }
+    do {
+      c = Serial.read();
+    } while (c != 'S' && c != '2');
+
+    if (c == '2') //version 2 bootloader
+    {
+      Serial.write(0xAA); //Send magic
+      while (Serial.read() != 'S');
+    }
+    
+    Serial.write(pages);
+    while (Serial.read() != 'P');
+    message = "reset";
+  }
+  else
+  {
+    bool repeat = true;
+    file.seek(step * PAGE_SIZE_BYTES);
+    char buffer[PAGE_SIZE_BYTES];
+    size_t bytesRead = file.readBytes(buffer, sizeof(buffer));
+
+    while (bytesRead < PAGE_SIZE_BYTES)
+      buffer[bytesRead++] = 0xff;
+    
+    uint32_t crc = crc32((uint32_t*)buffer, PAGE_SIZE_BYTES / 4, 0xffffffff);
+
+    while (repeat)
+    {
+      Serial.write(buffer, sizeof(buffer));
+      while (!Serial.available());
+      char res = Serial.read();
+
+      if ('C' == res) {
+        Serial.write((char*)&crc, sizeof(uint32_t));
+        while (!Serial.available());
+        res = Serial.read();
+      }
+
+      switch (res) {
+        case 'D':
+          message = "Update Done";
+          repeat = false;
+          fastUartAvailable = true;
+          break;
+        case 'E':
+          while (Serial.read() != 'T');
+          break;
+        case 'P':
+          message = "Page write success";
+          repeat = false;
+          break;
+        default:
+        case 'T':
+          break;
+      }
+    }
+  }
+  server.send(200, "text/json", "{ \"message\": \"" + message + "\", \"pages\": " + pages + " }");
+  file.close();
+}
+
+static void handleWifi()
+{
+  bool updated = true;
+  if(server.hasArg("apSSID") && server.hasArg("apPW")) 
+  {
+    WiFi.softAP(server.arg("apSSID").c_str(), server.arg("apPW").c_str());
+  }
+  else if(server.hasArg("staSSID") && server.hasArg("staPW")) 
+  {
+    WiFi.mode(WIFI_AP_STA);
+    WiFi.begin(server.arg("staSSID").c_str(), server.arg("staPW").c_str());
+  }
+  else
+  {
+    File file = SPIFFS.open("/wifi.html", "r");
+    String html = file.readString();
+    file.close();
+    html.replace("%staSSID%", WiFi.SSID());
+    html.replace("%apSSID%", WiFi.softAPSSID());
+    html.replace("%staIP%", WiFi.localIP().toString());
+    server.send(200, "text/html", html);
+    updated = false;
+  }
+
+  if (updated)
+  {
+    File file = SPIFFS.open("/wifi-updated.html", "r");
+    size_t sent = server.streamFile(file, getContentType("wifi-updated.html"));
+    file.close();    
+  }
+}
+
+static void handleBaud()
+{
+  if (fastUart)
+    server.send(200, "text/html", "fastUart on");
+  else
+    server.send(200, "text/html", "fastUart off");
+}
+
+void staCheck(){
+  sta_tick.detach();
+  if(!(uint32_t)WiFi.localIP()){
+    WiFi.mode(WIFI_AP); //disable station mode
+  }
+}
+
+void setup(void){
+  Serial.begin(115200);
+  Serial.setTimeout(100);
+  SPIFFS.begin();
+
+  //WIFI INIT
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.begin();
+  sta_tick.attach(10, staCheck);
+  
+  MDNS.begin(host);
+  
+  //SERVER INIT
+  ArduinoOTA.begin();
+  //list directory
+  server.on("/list", HTTP_GET, handleFileList);
+  //load editor
+  server.on("/edit", HTTP_GET, [](){
+    if(!handleFileRead("/edit.htm")) server.send(404, "text/plain", "FileNotFound");
+  });
+  //create file
+  server.on("/edit", HTTP_PUT, handleFileCreate);
+  //delete file
+  server.on("/edit", HTTP_DELETE, handleFileDelete);
+  //first callback is called after the request has ended with all parsed arguments
+  //second callback handles file uploads at that location
+  server.on("/edit", HTTP_POST, [](){ server.send(200, "text/plain", ""); }, handleFileUpload);
+
+  server.on("/wifi", handleWifi);
+  server.on("/cmd", handleCommand);
+  server.on("/fwupdate", handleUpdate);
+  server.on("/baud", handleBaud);
+
+  //called when the url is not defined here
+  //use it to load content from SPIFFS
+  server.onNotFound([](){
+    if(!handleFileRead(server.uri()))
+      server.send(404, "text/plain", "FileNotFound");
+  });
+
+  server.begin();
+  server.client().setNoDelay(1);
+}
+ 
+void loop(void){
+  server.handleClient();
+  ArduinoOTA.handle();
+}
