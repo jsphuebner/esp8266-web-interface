ecdf11a6c8025f5d5a857802bab573abb5238a6b
johannes <dev@johanneshuebner.com>
2024-03-18 21:49:56 +0100

    Now fully merged

--- data/inverter.js
+++ data/inverter.js
@@ -18,48 +18,112 @@
  *
  */
 
+
+/** @brief this is a little cache to store the current params/spot values. This
+ * is here so that different functions can do look-ups without making a full
+ * HTTP call to the inverter each time. */
+
+var paramsCache = {
+
+    data: undefined,
+    dataById: {},
+    failedFetchCount: 0,
+
+    get: function(name) {
+      if ( paramsCache.data !== undefined )
+      {
+        if ( name in paramsCache.data ) {
+          if ( paramsCache.data[name].enums ) {
+                return paramsCache.data[name].enums[paramsCache.data[name].value];
+            } else {
+              return paramsCache.data[name].value;
+            }
+        }
+      }
+      return null;
+    },
+
+    getEntry: function(name) {
+      return paramsCache.data[name];
+    },
+
+    getData: function() { return paramsCache.data; },
+
+    setData: function(data) {
+      paramsCache.data = data;
+
+      for (var key in data) {
+        paramsCache.dataById[data[key].id] = data[key];
+        paramsCache.dataById[data[key].id]['name'] = key;
+      }
+    },
+
+    getJson: function() { return JSON.stringify(paramsCache.data); },
+
+    getById: function(id) {
+      return paramsCache.dataById[id];
+    }
+}
+
 var inverter = {
+
 	firmwareVersion: 0,
-	
+
+	/** @brief send a command to the inverter */
 	sendCmd: function(cmd, replyFunc, repeat)
 	{
 		var xmlhttp=new XMLHttpRequest();
 		var req = "/cmd?cmd=" + cmd;
-		
-		xmlhttp.onload = function() 
+
+		xmlhttp.onload = function()
 		{
 			if (replyFunc) replyFunc(this.responseText);
 		}
-		
+
 		if (repeat)
 			req += "&repeat=" + repeat;
-		
+
 		xmlhttp.open("GET", req, true);
 		xmlhttp.send();
 	},
-	
+
+	/** @brief get the params from the inverter */
 	getParamList: function(replyFunc, includeHidden)
 	{
 		var cmd = includeHidden ? "json hidden" : "json";
-		
+
 		inverter.sendCmd(cmd, function(reply) {
 			var params = {};
-			try {
+			try
+			{
 				params = JSON.parse(reply);
-				
+
 				for (var name in params)
 				{
 					var param = params[name];
 					param.enums = inverter.parseEnum(param.unit);
-					
+
 					if (name == "version")
 						inverter.firmwareVersion = parseFloat(param.value);
 				}
-			} catch(ex) {}
+				paramsCache.failedFetchCount = 0;
+			}
+			catch(ex)
+			{
+        paramsCache.failedFetchCount += 1;
+        if ( paramsCache.failedFetchCount >= 2 ){
+          ui.showCommunicationErrorBar();
+        }
+			}
+			if ( paramsCache.failedFetchCount < 2 )
+			{
+				ui.hideCommunicationErrorBar();
+			}
+			paramsCache.setData(params);
 			if (replyFunc) replyFunc(params);
 		});
 	},
-	
+
 	getValues: function(items, repeat, replyFunc)
 	{
 		var process = function(reply)
@@ -71,7 +135,7 @@ var inverter = {
 			for (var res = expr.exec(reply); res; res = expr.exec(reply))
 			{
 				var val = parseFloat(res[1]);
-				
+
 				if (!values[items[signalIdx]])
 					values[items[signalIdx]] = new Array()
 				values[items[signalIdx]].push(val);
@@ -79,28 +143,105 @@ var inverter = {
 			}
 			replyFunc(values);
 		};
-		
+
 		if (inverter.firmwareVersion < 3.53 || items.length > 10)
 			inverter.sendCmd("get " + items.join(','), process, repeat);
 		else
 			inverter.sendCmd("stream " + repeat + " " + items.join(','), process);
 	},
-	
+
+
+	/** @brief given the 'unit' string provided by the inverter api, parse out
+	 * the key value pairs and return them in an array.
+	 * @param unit, e.g. "0=None, 1=UdcLow, 2=UdcHigh, 4=UdcBelowUdcSw"
+	 * Example return : ['None', 'UdcLow', 'UdcHigh',,'udcBelowUdcSw']. Note,
+	 * the extra comma is intentional. The position in the array is determined
+	 * by the index on the left hand side of the equals in the 'unit' string.
+	 */
 	parseEnum: function(unit)
 	{
 		var expr = /(\-{0,1}[0-9]+)=([a-zA-Z0-9_\-\.]+)[,\s]{0,2}|([a-zA-Z0-9_\-\.]+)[,\s]{1,2}/g;
 		var enums = new Array();
 		var res = expr.exec(unit);
-	
+
 		if (res)
 		{
 			do
 			{
 				enums[res[1]] = res[2];
 			} while (res = expr.exec(unit))
+			//console.log('enums : ' + enums);
 			return enums;
 		}
 		return false;
+	},
+
+	/** @brief helper function, from a list of parameters send parameter with given index to inverter
+	 * @param params map of parameters (name -> value)
+	 * @param index numerical index which parameter to set */
+	setParam: function(params, index)
+	{
+		var keys = Object.keys(params);
+
+		if (index < keys.length)
+		{
+			var key = keys[index];
+			modal.appendToModal('large', "Setting " + key + " to " + params[key] + "<br>");
+			inverter.sendCmd("set " + key + " " + params[key], function(reply) {
+				modal.appendToModal('large', reply + "<br>");
+				// auto-scroll text in modal as it is added
+				modal.largeModalScrollToBottom();
+				inverter.setParam(params, index + 1);
+			});
+		}
+	},
+
+    /** @brief Add/Delete a CAN mapping
+     * @param direction, tx, rx, or del
+     * @param name, spot value name
+     * @param id, canid of message
+     * @param pos, offset within frame
+     * @param bits, length of field
+     * @param gain, multiplier
+     */
+	canMapping: function(direction, name, id, pos, bits, gain)
+	{
+		var cmd = "can " + direction + " " + name + " " + id + " " + pos + " " + bits + " " + gain;
+		inverter.sendCmd(cmd);
+	},
+
+    /** @brief get a list of files in the spiffs filesystem on the esp8266 */
+	getFiles: function(replyFunc)
+	{
+		var filesRequest = new XMLHttpRequest();
+		filesRequest.onload = function()
+		{
+			var filesJson = JSON.parse(this.responseText);
+			replyFunc(filesJson);
+		}
+		filesRequest.onerror = function()
+		{
+			alert("error");
+		}
+		filesRequest.open("GET", "/list", true);
+		filesRequest.send();
+	},
+
+	/** @brief delete a file from the spiffs filessytem on the esp8266 */
+	deleteFile: function(filename, replyFunc)
+	{
+		var deleteFileRequest = new XMLHttpRequest();
+		deleteFileRequest.onload = function()
+		{
+			var responseJson = JSON.parse(this.responseText);
+			replyFunc(responseJson);
+		}
+		deleteFileRequest.onerror = function()
+		{
+			alert("error");
+		}
+		deleteFileRequest.open("DELETE", "/edit?f=" + filename, true);
+		deleteFileRequest.send();
 	}
 
 
--- data/log.js
+++ data/log.js
@@ -18,99 +18,146 @@
  *
  */
 
-var items = [];
-var textArea;
-var minmax = false;
+var log = {
 
-function onLoad()
-{
-	inverter.firmwareVersion = 4;
-	inverter.getParamList();
-}
+	items: [],
+	samples: 0,
+	textArea: undefined,
+	minmax: false,
+	stopLogging: true,
 
-function start()
-{
-	if (items.length)
+    /* @brief add field to data logger */
+	addLogItem: function()
 	{
-		items = [];
-		return;
-	}
-	
-	textArea = document.getElementById("textArea");
-	var samples = document.getElementById("samples").value;
-	var paramPart = document.location.href.split("items=");
-	items = paramPart[1].split(",");
-	minmax = document.getElementById("minmax").checked;
-	
-	textArea.innerHTML = "Timestamp"
-	
-	if (minmax)
-	{
-		for (var i = 0; i < items.length; i++)
+	  	var dataLoggerConfiguration = document.getElementById('data-logger-configuration');
+
+	  	// container for the drop down and the delete button
+		var selectDiv = document.createElement("div");
+		selectDiv.classList.add('logger-field');
+		dataLoggerConfiguration.appendChild(selectDiv);
+
+		// Create a drop down and populate it with the possible spot values
+		var selectSpotValue = document.createElement("select");
+		selectSpotValue.classList.add('logger-field-select');
+		for ( var key in paramsCache.getData() )
 		{
-			textArea.innerHTML += "," + items[i] + " (avg)," + items[i] + " (min)," + items[i] + " (max)";
-		}
-	}
-	else
-	{
-		textArea.innerHTML += "," + paramPart[1];
-	}
-	
-	textArea.innerHTML += "\r\n";
-	
-	acquire(samples);
-}
-
-function save()
-{
-  var textToWrite = document.getElementById('textArea').innerHTML;
-  var textFileAsBlob = new Blob([ textToWrite ], { type: 'text/csv' });
-  var fileNameToSaveAs = "log.csv";
-
-  var downloadLink = document.createElement("a");
-  downloadLink.download = fileNameToSaveAs;
-  downloadLink.innerHTML = "Download File";
-  if (window.webkitURL != null) {
-    // Chrome allows the link to be clicked without actually adding it to the DOM.
-    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
-  } else {
-    // Firefox requires the link to be added to the DOM before it can be clicked.
-    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
-    downloadLink.onclick = function(event) {document.body.removeChild(event.target)};
-    downloadLink.style.display = "none";
-    document.body.appendChild(downloadLink);
-  }
-
-  downloadLink.click();
-}
-
-function acquire(samples)
-{
-	if (!items.length) return;
-
-	inverter.getValues(items, samples,
-	function(values) 
-	{
-		var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
-		var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
-		var line = localISOTime;
-		for (var name in values)
-		{
-			var avg = values[name].reduce((acc, c) => acc + c, 0) / samples;
-			
-			if (minmax)
+			if ( ! paramsCache.getEntry(key).isparam )
 			{
-				line += "," + avg.toFixed(2) + "," + Math.min(...values[name]) + "," + Math.max(...values[name]);
-			}
-			else
-			{
-				line += "," + avg.toFixed(2);
+				var option = document.createElement("option");
+				option.value = key;
+				option.text = key;
+				selectSpotValue.appendChild(option);
 			}
 		}
-		line += "\r\n";
-		textArea.innerHTML += line;
-		textArea.scrollTop = textArea.scrollHeight;
-		acquire(samples);
-	});
-}
+		selectDiv.appendChild(selectSpotValue);
 
+		// Add the delete button
+		var deleteButton = document.createElement("button");
+		var deleteButtonImg = document.createElement('img');
+		deleteButtonImg.src = '/icon-trash.png';
+		deleteButton.appendChild(deleteButtonImg);
+		deleteButton.onclick = function() { this.parentNode.remove(); };
+		selectDiv.appendChild(deleteButton);
+	},
+
+    /** @brief return a list of fields currently configured for logger */
+	getLogItems: function()
+	{
+	  	log.items = [];
+		var formItems = document.forms["data-logger-configuration"].elements;
+		for ( var i = 0; i < formItems.length; i++ )
+		{
+			if ( formItems[i].type === 'select-one' && formItems[i].classList.contains('logger-field-select') )
+			{
+			  	log.items.push(formItems[i].value);
+			}
+		}
+	},
+
+    /* @brief start collecting log data */
+	start: function()
+	{
+	    log.stopLogging = false;
+        log.getLogItems();
+		log.textArea = document.getElementById("data-logger-text-area");
+		log.samples = document.getElementById("data-logger-samples").value;
+		log.minmax = document.getElementById("data-logger-minmax").checked;
+		log.textArea.innerHTML = "Timestamp"
+
+		if (log.minmax)
+		{
+			for (var i = 0; i < log.items.length; i++)
+			{
+				log.textArea.innerHTML += "," + log.items[i] + " (avg)," + log.items[i] + " (min)," + log.items[i] + " (max)";
+			}
+		}
+		else
+		{
+			log.textArea.innerHTML += "," + log.items;
+	    }
+
+		log.textArea.innerHTML += "\r\n";
+		log.acquire(log.samples);
+	},
+
+	stop: function()
+	{
+		log.stopLogging = true;
+	},
+
+	save: function()
+	{
+		var textToWrite = document.getElementById('data-logger-text-area').innerHTML;
+		var textFileAsBlob = new Blob([ textToWrite ], { type: 'text/csv' });
+		var fileNameToSaveAs = "log.csv";
+
+		var downloadLink = document.createElement("a");
+		downloadLink.download = fileNameToSaveAs;
+	    downloadLink.innerHTML = "Download File";
+		if (window.webkitURL != null)
+		{
+			// Chrome allows the link to be clicked without actually adding it to the DOM.
+			downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+		} else {
+			// Firefox requires the link to be added to the DOM before it can be clicked.
+			downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+			downloadLink.onclick = function(event) {document.body.removeChild(event.target)};
+			downloadLink.style.display = "none";
+			document.body.appendChild(downloadLink);
+		}
+
+		downloadLink.click();
+	},
+
+	acquire: function(samples)
+	{
+		if ( log.stopLogging ){ return; }
+
+		if (!log.items.length) return;
+
+		inverter.getValues(log.items, log.samples,
+			function(values)
+			{
+				var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+				var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
+				var line = localISOTime;
+				for (var name in values)
+				{
+					var avg = values[name].reduce((acc, c) => acc + c, 0) / log.samples;
+
+					if (log.minmax)
+					{
+						line += "," + avg.toFixed(2) + "," + Math.min(...values[name]) + "," + Math.max(...values[name]);
+					}
+					else
+					{
+						line += "," + avg.toFixed(2);
+					}
+				}
+				line += "\r\n";
+				log.textArea.innerHTML += line;
+			    log.textArea.scrollTop = log.textArea.scrollHeight;
+				log.acquire(samples);
+			});
+	},
+}
